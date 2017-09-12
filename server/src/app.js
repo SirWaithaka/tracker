7554cf059332f652517474564cce1d3212ae9cad
craftsmon <craftsmon@outlook.com>
2017-09-12 15:47:10 +0300

    Add endpoint for register

---
+++ server/src/app.js
@@ -0,0 +1,18 @@
+const cors = require('cors')
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.post('/register', (req, res) => {
+  res.send({
+    message: `${req.body.email} has been registered`
+  })
+})
+
+module.exports = app
