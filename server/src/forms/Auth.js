f358c013f040847436efc7e5e949f20931783aed
craftsmon <craftsmon@outlook.com>
2017-09-15 16:07:31 +0300

    Added logic for Login route {Allows for login of User}

--- server/src/forms/Auth.js
+++ server/src/forms/Auth.js
@@ -1,6 +1,44 @@
 const { User } = require('../models')
+const jwt = require('jsonwebtoken')
+const config = require('../config')
+
+function jwtSignUser(user) {
+  const ONE_WEEK = 60 * 60 * 24 * 7
+  return jwt.sign(user, config.authentication.jwtSecret, {
+    expiresIn: ONE_WEEK
+  })
+}
 
 module.exports = {
+  async login (req, res) {
+    try {
+      const {email, password} = req.body
+
+      const user = await User.findOne({
+        where: {
+          email: email
+        }
+      })
+      if (!user) {
+        return res.status(403).send({ error: 'The login credentials were incorrect' })
+      }
+
+      const isValidPassword = (password === user.password)
+      if (!isValidPassword)
+        return res.status(403).send({ error: 'Username or password you provided is incorrect'})
+
+      res.status(200).send({
+        user: user.toJSON(),
+        token: jwtSignUser(user.toJSON())
+      })
+
+    }
+    catch (err) {
+      res.status(500).send(
+        { error:'Internal server error has occurred' }
+      )
+    }
+  },
   async register (req, res) {
     try {
       const user = await User.create(req.body)
@@ -11,6 +49,5 @@ module.exports = {
         {error:'Email account is already in use'}
       )
     }
-
   }
 }
