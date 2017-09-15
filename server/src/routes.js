5ec14e79b54213d04f9b3e221d34389c91e82345
craftsmon <craftsmon@outlook.com>
2017-09-15 16:16:04 +0300

    Added middleware to register route, Added Login route

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,5 +1,7 @@
 const Auth = require('./forms/Auth')
+const AuthPolicy = require('./policies/AuthPolicy')
 
 module.exports = (app) => {
-  app.post('/register', Auth.register)
+  app.post('/register', AuthPolicy.register, Auth.register)
+  app.post('/login', Auth.login)
 }
