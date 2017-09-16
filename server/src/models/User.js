cd96ca945f446142230ce44265aa4982e473a4a8
craftsmon <craftsmon@outlook.com>
2017-09-16 15:40:09 +0300

    Added promises and bcrypt, enabled hashing of password and added a prototype to the User model to enable comparison of password against a hashed password, also added hooks to the model

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,5 +1,22 @@
-module.exports = (sequelize, DataTypes) =>
-  sequelize.define('User', {
+const Promise = require('bluebird')
+const bcrypt = Promise.promisifyAll(require('bcrypt-nodejs'))
+
+function hashPassword (user, options) {
+  const SALT_FACTOR = 8
+
+  if (!user.changed('password'))
+    return;
+
+  return bcrypt
+    .genSaltAsync(SALT_FACTOR)
+    .then(salt => bcrypt.hashAsync(user.password, salt, null))
+    .then(hash => {
+      user.setDataValue('password', hash)
+    })
+}
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
     email: {
       allowNull: false,
       type: DataTypes.STRING,
@@ -9,4 +26,17 @@ module.exports = (sequelize, DataTypes) =>
       allowNull: false,
       type: DataTypes.STRING
     }
-  })
+ }, {
+   hooks: {
+     beforeCreate: hashPassword,
+     beforeUpdate: hashPassword,
+     beforeSave: hashPassword
+   }
+ })
+
+ User.prototype.comparePassword = function (password) {
+   return bcrypt.compareAsync(password, this.password)
+ }
+
+ return User
+}
