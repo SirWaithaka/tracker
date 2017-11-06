d325e24c29a826e19ff48def5fab5a4feb2dc1db
craftsmon <craftsmon@outlook.com>
2017-11-06 14:21:09 +0300

    Added Home link to btn Tab Tracker

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,10 +1,17 @@
 <template>
   <v-toolbar fixed dark class="teal">
     <v-toolbar-title class="mr-4">
-      Tab Tracker
+      <span
+        class="home"
+        @click="navigateTo({ name: 'root' })">
+        Tab Tracker
+      </span>
     </v-toolbar-title>
+
     <v-toolbar-items>
-      <v-btn flat dark>Browse</v-btn>
+      <v-btn flat dark>
+        Browse
+      </v-btn>
     </v-toolbar-items>
 
     <v-spacer></v-spacer>
@@ -15,10 +22,17 @@
        @click="navigateTo({name: 'Login'})">
        Login
       </v-btn>
+
       <v-btn flat dark
       v-if="!$store.state.isUserLoggedIn"
        @click="navigateTo({name: 'Register'})">
         Sign Up
+      </v-btn>
+
+      <v-btn flat dark
+      v-if="$store.state.isUserLoggedIn"
+       @click="logout">
+        Log Out
       </v-btn>
     </v-toolbar-items>
   </v-toolbar>
@@ -29,6 +43,14 @@
     methods: {
       navigateTo (route) {
         this.$router.push(route)
+      },
+      logout () {
+        this.$store.dispatch('setToken', null)
+        this.$store.dispatch('setUser', null)
+        // REDIRECT to Home
+        this.$router.push({
+          name: 'root'
+        })
       }
     }
   }
